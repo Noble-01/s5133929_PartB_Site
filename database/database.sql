dfb707f9c2ee7062ac27abcbbe43f3574e4c9bc1
Noble-01 <zaccripps5@gmail.com>
2020-05-11 02:00:24 +1000

    updated sql for db

--- database/database.sql
+++ database/database.sql
@@ -19,18 +19,19 @@ INSERT INTO Customer  VALUES
 CREATE TABLE Post
 (
     PID INTEGER PRIMARY KEY,
-    DISHNAME VARCHAR(30),
+    DISHNAME VARCHAR(30) NOT NULL,
+    LOCATION VARCHAR (40) NOT NULL, 
     CATEGORY VARCHAR(64) NOT NULL,
-    IMAGE BLOB NOT NULL,
-    RATING INTEGER (4) NOT NULL,
+    IMAGE BLOB,
+    RATING INTEGER (1),
     DISHDESCRIPTION VARCHAR(64) NOT NULL,
     CID INTEGER,
     FOREIGN KEY (CID) REFERENCES Customer(CID)
 );
 
 INSERT INTO Post VALUES
-(1,'steak n fires', 'steak', '', 3, 'good steak with nice sauce', 1),
-(2,'Nigiri', 'Sushi', '', 4, 'fish was fresh', 1)
+(1,'steak n fires', 'Gold Coast','steak', '', 3, 'good steak with nice sauce', 1),
+(2,'Nigiri','Cheeky Moneky, Brisbane' ,'Sushi', '', 4, 'fish was fresh', 1)
 ;
 
 CREATE TABLE Question
