610cc4633dd9cd69f30cb61795ca7e6d1fc9aee0
Noble-01 <zaccripps5@gmail.com>
2020-05-14 15:16:33 +1000

    added comments

--- PHP/categoryQueries.php
+++ PHP/categoryQueries.php
@@ -1,13 +1,15 @@
 <?php
-
+//this php file was created for the php pages in the sub folder Categories as they can not connect to the db due to a different filepath
 class MyDB extends SQLite3
    {
+        //function to create connection to db
       function __construct()
       {
+         //open db at file path
             $this->open('..\..\database\database.db');
       }
    }
-   
+   //function returns posts from the table Post for food pages in the sub folder Categories
    function getPosts($searchTerm = null){
       $db = new MyDB();
       $array = [];
@@ -16,21 +18,29 @@ class MyDB extends SQLite3
       } else {
          //echo "Opened database successfully\n";
       }
+      //if no arguement is given for the function return all records from table Post
       if(!$searchTerm) {
          $sql ='SELECT * from Post;';
       } else {
+         //return records from table Post with the same category of the page e.g. Pizza, Sushi, Steak, Enchilada
          $sql ='SELECT * FROM Post WHERE CATEGORY LIKE "'.$searchTerm.'"';
       }
+      //retrieve returned results
       $ret = $db->query($sql);
      
+      //if no results are returned display error message
       if(!$ret){
         echo $db->lastErrorMsg();
+        //return empty array
         return [];
       } else {
+         //let returned records be placed into an array
          while($row = $ret->fetchArray(SQLITE3_ASSOC) ){
             $array[] = $row;
          }
+         //close database
          $db->close();
+         //return array with 4 posts
          return $array;
       }
    }
--- PHP/query.php
+++ PHP/query.php
@@ -2,29 +2,33 @@
 
    class MyDB extends SQLite3
    {  
+      //function to create connection to db
       function __construct()
       {
+         //open db at file path
             $this->open('..\database\database.db');
       }
    }
-   
+   //function is used to add customers to the table Customer
    function addCustomer($customerName) {
-      
+      //create new instance of class
       $db = new MyDB();
       if(!$db){
          echo '<script type="text/javascript">alert("'.$db->lastErrorMsg().'");</script>';
       } else {
-         //echo "Opened database successfully\n";
+         //echo "Opened database successfully";
       }
+         //insert customer into table if he doesn't exist already in the db
          $sql =   'INSERT INTO Customer (NAME) 
                   SELECT * FROM (SELECT "'.$customerName.'") AS TEMP 
                   WHERE NOT EXISTS 
                   ( SELECT NAME FROM Customer WHERE NAME = "'.$customerName.'") 
                   LIMIT 1 ;';
+         //execute query in the database
          $db->query($sql);
    }
    
-   
+   // function adds posts to table Post
    function addPost( $customerName,$dishName, $location, $submitCategory, $file, $rating, $submitDescription) {
       
       $db = new MyDB();
@@ -33,12 +37,14 @@
       } else {
          //echo "Opened database successfully\n";
       }
-  
-      $sql ='INSERT INTO Post (DISHNAME, LOCATION,CATEGORY,IMAGE,RATING, DISHDESCRIPTION, CID) VALUES ("'.$dishName.'","'.$location.'", "'.$submitCategory.'", "'.$file.'","'.$rating.'","'.$submitDescription.'", (SELECT CID FROM Customer WHERE NAME = "'.$customerName.'"));';
+      //insert post into table with foriegn key of the customer that has submitted the post
+      $sql ='INSERT INTO Post (DISHNAME, LOCATION,CATEGORY,IMAGE,RATING, DISHDESCRIPTION, CID) 
+            VALUES ("'.$dishName.'","'.$location.'", "'.$submitCategory.'", "'.$file.'","'.$rating.'","'.$submitDescription.'", 
+            (SELECT CID FROM Customer WHERE NAME = "'.$customerName.'"));';
       $db->query($sql);
    }
    
-   
+   //function adds questions to the table Question
    function addQuestion($customerContactName, $contactEmail, $subject, $questionDescription) {
       
       $db = new MyDB();
@@ -47,14 +53,18 @@
       } else {
          //echo "Opened database successfully\n";
       }
+      //query to check if customer already exists in the Customer table
       $check = 'SELECT NAME FROM Customer WHERE NAME = "'.$customerContactName.'";';
       $result = $db -> query($check);
+      //if a record is returned then the customer must already be registered
+      //update customers email in Customer table
       if ($result){
       $sql = 'UPDATE Customer
                SET EMAIL = "'.$contactEmail.'"
                WHERE NAME = "'.$customerContactName.'";';
       $db -> query($sql);
       }
+      //if customer is not in table then add his name and email to Customer table where name doesn't exist
       else{
          $sql =   'INSERT INTO Customer (NAME, EMAIL) 
          SELECT * FROM (SELECT "'.$customerContactName.'", "'.$contactEmail.'") AS TEMP 
@@ -63,12 +73,14 @@
          LIMIT 1 ;';
          $db->query($sql);
       }
+      //insert question into table Question
       $sql ='INSERT INTO Question (SUBJECT, QUESTIONDESCRIPTION, CID) VALUES ("'.$subject.'", "'.$questionDescription.'",  (SELECT CID FROM Customer WHERE NAME = "'.$customerContactName.'"));';
       $db->query($sql);
 
      
    }
    
+   //function retrieves 4 random records from Post table for the index.php
    function getRandomRecords($searchTerm = null){
       $db = new MyDB();
       $array = [];
@@ -77,22 +89,31 @@
       } else {
          //echo "Opened database successfully\n";
       }
+      //return 4 random records from Post
       if(!$searchTerm) {
          $sql ='SELECT * FROM Post ORDER BY RANDOM() LIMIT 4';
 
       } else {
+         //return all records from Post
+         //shouldn't really do this but just in case
          $sql ='SELECT * from Post;';
       }
+      //retrieve returned results
       $ret = $db->query($sql);
      
+      //if no results are returned display error message
       if(!$ret){
         echo $db->lastErrorMsg();
+        //return empty array
         return [];
       } else {
+         //let returned records be placed into an array
          while($row = $ret->fetchArray(SQLITE3_ASSOC) ){
             $array[] = $row;
          }
+         //close database
          $db->close();
+         //return array with 4 posts
          return $array;
       }
    }
