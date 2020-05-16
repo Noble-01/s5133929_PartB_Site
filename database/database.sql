436f7df0f9b42906b13c05229eff565398b97cda
Noble-01 <zaccripps5@gmail.com>
2020-05-17 00:18:53 +1000

    added more data to insert

--- database/database.sql
+++ database/database.sql
@@ -8,12 +8,13 @@ CREATE TABLE Customer
     CID INTEGER PRIMARY KEY,
     NAME VARCHAR(64) NOT NULL,
     EMAIL VARCHAR(64)
-
 );
 
 INSERT INTO Customer  VALUES
 (1, 'Allan McGregor', 'Allan@gmail.com'),
-(2, 'XRay Zulu', 'XZ@gmail.com');
+(2, 'Zachary Cripps', 'email@email.com'),
+(3, 'Sofie Cripps', 'testingEmail5@email.com'),
+(4, 'XRay Zulu', '');
 
 CREATE TABLE Post
 (
@@ -30,8 +31,9 @@ CREATE TABLE Post
 
 INSERT INTO Post VALUES
 (1,'steak n fires', 'Gold Coast','steak', '', 3, 'good steak with nice sauce', 1),
-(2,'Nigiri','Cheeky Moneky, Brisbane' ,'Sushi', '', 4, 'fish was fresh', 1)
-;
+(2,'Nigiri','Cheeky Moneky, Brisbane' ,'Sushi', '', 4, 'fish was fresh', 2),
+(3,'deep fry pizza','Joes Pizza, Brisbane' ,'Pizza', '', 5, 'Simply awesome and tasty', 3),
+(4,'Nigiri','Cheeky Moneky, Brisbane' ,'Sushi', '', 4, 'fish was fresh', 1);
 
 CREATE TABLE Question
 (
@@ -44,4 +46,5 @@ CREATE TABLE Question
 
 INSERT INTO Question VALUES
 (1, 'wesbite broke', 'when i click on the search bar the website shits the bed', 2),
-(2, 'still broken', 'I am good at complaining', 2);
+(2, 'still broken', 'I am good at complaining', 2),
+(3, 'can"t post anyting', 'I click submit and it does nothing and stats on the same page', 1);
